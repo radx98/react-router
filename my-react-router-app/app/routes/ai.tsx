d09926b55e58e83c859c9160fdfc46189e4fe7fd
Boris Ostanin <radx98@gmail.com>
2025-10-01 17:57:51 -0400

    handler set up

---
+++ my-react-router-app/app/routes/ai.tsx
@@ -0,0 +1,16 @@
+import {createOpenAI} from "@ai-sdk/openai"
+import type { Route } from "./+types/home"
+import { streamText, convertToModelMessages, type UIMessage } from "ai"
+
+export const maxDuration = 30
+
+const openai = createOpenAI({apiKey: process.env.OPENAI_API_KEY})
+
+export async function action({ request }: Route.ActionArgs) {
+    const { messages }: { messages: UIMessage[] } = await request.json()
+    const result = streamText({
+        model: openai("gpt-5-mini"),
+        messages: convertToModelMessages(messages)
+    })
+    return result.toUIMessageStreamResponse()
+}
